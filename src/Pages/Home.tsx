839602bc220aedf6793a628491c23030d7c346ba
Raiyan <raiyan5764@gmail.com>
2022-02-23 21:39:12 +0600

    todo delete functionalites done

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,8 +2,8 @@ import cogoToast from "cogo-toast";
 import { nanoid } from "nanoid";
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 import Todo from "./SingalTodo";
-const randomColor = require("randomcolor");
 
 interface formData {
   id: string;
@@ -15,21 +15,25 @@ interface formData {
 
 const Home = () => {
   const { register, handleSubmit, reset } = useForm<formData>();
+  const [switchEdit, setSwitchEdit] = useState<boolean>(false);
+  const [editData, setEditData] = useState<formData>({} as formData);
+  // todo states
   const [todos, setTodos] = useState<formData[]>(
     JSON.parse(localStorage.getItem("todos")!) || []
   );
-  const myColors = randomColor({
-    luminosity: "light",
-    hue: "green", // e.g. 'rgb(225,200,20)'
-  });
 
+  // generate random colors
+  const myRandomColors: string[] = ["#FB7C72", "#32FBDC", "#894AEC", "#FFDA45"];
+
+  // save the todo in the local storage
   const onSubmit = (data: formData) => {
-    data.bg = myColors;
+    data.bg = myRandomColors[Math.floor(Math.random() * 5)];
     data.id = nanoid(10);
     data.date = new Date().toISOString();
     const newData = [...todos, data].reverse();
     setTodos(newData);
     cogoToast.success("YAY!!🥳🎉 You added a todo");
+
     reset();
   };
 
@@ -38,39 +42,101 @@ const Home = () => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
 
+  // delete a todo
+  const handelDelete = (selectedId: string): void => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You want to delete this todo?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const afterDeletedTodo = todos.filter(({ id }) => id !== selectedId);
+        setTodos(afterDeletedTodo);
+      } else {
+        Swal.fire("Ok no problem", "", "info");
+      }
+    });
+  };
+
   return (
     <section className="lg:max-w-6xl lg:mx-auto ">
       <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cold-1 px-4 mt-10">
-        <form
-          className="flex flex-col  w-full space-y-5 "
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <h1 className="text-center font-semibold text-3xl text-green-500">
-            📜📜 Write Todo 📜📜
-          </h1>
-          <input
-            required
-            className="outline-none border-2 border-gray-400 bg-white shadow focus:shadow-lg transition-all focus:ring-0 py-4 rounded-lg"
-            type="text"
-            placeholder="Write a title..."
-            {...register("title")}
-          />
-          <textarea
-            required
-            className="outline-none border-2 border-gray-400 bg-white shadow focus:shadow-lg transition-all  focus:ring-0 py-4 rounded-lg lg:h-[200px] resize-none md:h-[200px] h-full w-full"
-            placeholder="Start writing..."
-            {...register("des")}
-          ></textarea>
-          <button className="bg-green-500 text-white py-2 rounded font-semibold hover:bg-white hover:text-green-500 transition-all hover:border-green-500 border-2 border-transparent">
-            Submit
-          </button>
-        </form>
+        {switchEdit ? (
+          <form
+            className="flex flex-col  w-full space-y-5 "
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <h1 className="text-center font-semibold text-3xl text-green-500">
+              📜📜 Edit Todo 📜📜
+            </h1>
+            <input
+              required
+              className="outline-none border-2 border-gray-400 bg-white shadow focus:shadow-lg transition-all focus:ring-0 py-4 rounded-lg"
+              type="text"
+              defaultValue={editData?.title}
+              placeholder="Write a title..."
+              {...register("title")}
+            />
+            <textarea
+              required
+              className="outline-none border-2 border-gray-400 bg-white shadow focus:shadow-lg transition-all  focus:ring-0 py-4 rounded-lg lg:h-[200px] resize-none md:h-[200px] h-full w-full"
+              defaultValue={editData?.des}
+              placeholder="Start writing..."
+              {...register("des")}
+            ></textarea>
+            <div className="flex space-x-4">
+              <button className="edit_btn">Update todo</button>
+              <button
+                onClick={() => {
+                  setSwitchEdit(false);
+                  setEditData({} as formData);
+                }}
+                className="delete_btn"
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        ) : (
+          <form
+            className="flex flex-col  w-full space-y-5 "
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <h1 className="text-center font-semibold text-3xl text-green-500">
+              📜📜 Write Todo 📜📜
+            </h1>
+            <input
+              required
+              className="outline-none border-2 border-gray-400 bg-white shadow focus:shadow-lg transition-all focus:ring-0 py-4 rounded-lg"
+              type="text"
+              defaultValue=""
+              placeholder="Write a title..."
+              {...register("title")}
+            />
+            <textarea
+              required
+              className="outline-none border-2 border-gray-400 bg-white shadow focus:shadow-lg transition-all  focus:ring-0 py-4 rounded-lg lg:h-[200px] resize-none md:h-[200px] h-full w-full"
+              placeholder="Start writing..."
+              defaultValue=""
+              {...register("des")}
+            ></textarea>
+            <button className="bg-green-500 text-white py-2 rounded font-semibold hover:bg-white hover:text-green-500 transition-all hover:border-green-500 border-2 border-transparent">
+              Add todo
+            </button>
+          </form>
+        )}
         {/* Show todos in the UI */}
-        <div className="h-[500px] overflow-y-scroll scrollbar-hide mt-20 lg:mt-0 md:mt-0">
-          <h1 className="text-center text-green-500 text-2xl">Your todos</h1>
-          <div className="text-sm lg:mx-8 md:mx-4 space-y-5">
+        <div className=" my-20 lg:mt-0 md:mt-0">
+          <h1 className="text-center text-green-500 text-2xl font-semibold">
+            Tasks
+          </h1>
+          <div className="h-[500px] overflow-y-scroll scrollbar-hide text-sm lg:mx-8 md:mx-4 space-y-5">
             {todos.map((todo) => (
-              <Todo key={todo.id} todo={todo} />
+              <Todo key={todo.id} todo={todo} handelDelete={handelDelete} />
             ))}
           </div>
         </div>
--- src/Pages/SingalTodo.tsx
+++ src/Pages/SingalTodo.tsx
@@ -10,23 +10,28 @@ interface data {
     date: string;
     bg: string;
   };
+  handelDelete: (id: string) => void;
 }
 
-const Todo = ({ todo }: data) => {
+const Todo = ({ todo, handelDelete }: data) => {
   console.log(todo);
 
   return (
     <div
-      className=" p-3 rounded-lg border-2 shadow-lg border-gray-400 mt-7"
+      className=" p-3 rounded-lg hover:shadow shadow-gray-200 shadow-lg duration-700 transition-all mt-7"
       style={{ backgroundColor: `${todo.bg}` }}
     >
       <div className="flex items-center justify-between">
-        <h2 className="font-semibold text-lg text-gray-800">{todo.title}</h2>
-        <div className="text-xl space-x-1">
-          <button className="text-red-400 hover:scale-110 transition-all">
+        <h2 className="font-semibold text-xl text-white">{todo.title}</h2>
+        {/* edit and delete buttons */}
+        <div className="text-2xl space-x-1">
+          <button
+            onClick={() => handelDelete(todo.id)}
+            className="text-white hover:scale-110 transition-all"
+          >
             <TiDelete />
           </button>
-          <button className="text-blue-500 hover:scale-110 transition-all">
+          <button className="text-white hover:scale-110 transition-all">
             <TiEdit />
           </button>
         </div>
@@ -34,10 +39,10 @@ const Todo = ({ todo }: data) => {
 
       <textarea
         disabled
-        className="outline-none text-sm h-[100px] scrollbar-hide bg-transparent resize-none border-none w-full p-0 my-3"
+        className="outline-none text-sm h-[100px] scrollbar-hide bg-transparent resize-none border-none w-full p-0 my-3 text-white"
         defaultValue={todo.des}
       ></textarea>
-      <p className="text-right text-xs text-gray-600 font-semibold my-2">
+      <p className="text-right text-xs text-gray-100 font-semibold my-2">
         <Moment fromNow>{todo.date}</Moment>
       </p>
     </div>
